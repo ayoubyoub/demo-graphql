7405a1801c6e3eb6bfac9bbd583c046cc5ca7290
AYOUB YOUB <contact@howtodemy.com>
2021-04-12 18:20:41 +0000

    add graphql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
-
-const typeDefs = require("./graphql/typeDefs");
+const fs = require("fs");
+const path = require("path");
 const resolvers = require("./graphql/resolvers");
 
 // instantiate instance of PubSub()
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 // setup apollo server
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, "./graphql/schema.graphql"),
+    "utf8"
+  ),
   resolvers,
   // callback. this is middleware so we get the req/res from express and then forward it to the context, which we can then access in graphql posts resolver
   // pass pubsub instance to context so we can use it in our resolvers
